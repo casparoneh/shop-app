f3715794ec2a81e5259e891e6de65934b113df73
Lorik Vitia <yamada_lorri@hotmail.com>
2023-12-16 21:58:00 +0100

    adding change passwrod to account

--- frontend/src/app/myaccount/changepassword/page.js
+++ frontend/src/app/myaccount/changepassword/page.js
@@ -1,6 +1,67 @@
+"use client";
+
+import Button from "@/components/utils/Button";
+import MenuContainer from "@/components/utils/MenuContainer";
+import TextInput from "@/components/utils/TextInput";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+
 const ChangePassword = () => {
+  const globalValues = {
+    oldPassword: "",
+    newPassword: "",
+  };
+
+  const MessageValidation = Yup.object({
+    oldPassword: Yup.string()
+      .required("Old Password is required")
+      .min(
+        8,
+        "Passwords must contain at least 8 characters, 1 uppercase letter, 1 digit and 1 special character"
+      ),
+    newPassword: Yup.string()
+      .required("New Password is required")
+      .min(
+        8,
+        "Passwords must contain at least 8 characters, 1 uppercase letter, 1 digit and 1 special character"
+      ),
+  });
+
+  const onSubmit = async (values, onSubmitProps) => {
+    try {
+      console.log("asd");
+      onSubmitProps.resetForm();
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  };
+
   return (
-    <div>ChangePassword</div>
-  )
-}
-export default ChangePassword
+    <div>
+      <MenuContainer>
+        <Formik
+          initialValues={globalValues}
+          validationSchema={MessageValidation}
+          onSubmit={onSubmit}
+          enableReinitialize
+        >
+          <Form className="w-full">
+            <TextInput
+              name="oldPassword"
+              placeholder="Old password"
+              type="password"
+            />
+            <TextInput
+              name="newPassword"
+              placeholder="New password"
+              type="password"
+            />
+
+            <Button title="Send" type="submit" />
+          </Form>
+        </Formik>
+      </MenuContainer>
+    </div>
+  );
+};
+export default ChangePassword;
