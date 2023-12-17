d1a5a3f3e57b4a9bae4143601688930352a6e7d6
Lorik Vitia <yamada_lorri@hotmail.com>
2023-12-17 18:22:27 +0100

    active to account

--- frontend/src/components/utils/MenuContainer.js
+++ frontend/src/components/utils/MenuContainer.js
@@ -1,11 +1,33 @@
+"use client";
+
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 const MenuContainer = ({ children }) => {
+  const [activeMenu, setActiveMenu] = useState("orders");
+
+  const router = usePathname();
+
+  useEffect(() => {
+    const currentPath = router;
+
+    setActiveMenu(currentPath.replace(/^\/myaccount\//, ""));
+  }, [router.pathname]);
+
   return (
     <div className="container px-4 md:px-0 mx-auto flex flex-col md:flex-row justify-between w-full py-20">
       {/* LEFT SIDE */}
       <div className="w-full md:w-1/3 mb-4 md:mb-0">
-        <h1 className="text-3xl font-black">MENU CONTAINER</h1>
+        {/* activeMenu.toUpperCase() */}
+        <h1 className="text-3xl font-black">
+          {activeMenu == "changepassword"
+            ? "CHANGE PASSWORD"
+            : activeMenu == "accountdetails"
+            ? "ACCOUNT DETAILS"
+            : activeMenu?.toUpperCase()}
+        </h1>
         <div className="flex flex-col gap-6 font-bold text-sm my-8">
           <Link href="/myaccount/orders">ORDERS</Link>
           <Link href="/myaccount/accountdetails">ACCOUNT DETAILS</Link>
